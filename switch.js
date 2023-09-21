74b6a29744270dbfcbcc21d3e92a88155d64ec9d
Vincent Mukuna <mukunavincent28@gmail.com>
2023-09-21 23:58:07 +0300

    l

--- switch.js
+++ switch.js
@@ -3,9 +3,7 @@ import { secureChatDoc, clearChatMessages } from "./chats.js";
 import { addToGlobalChat } from "./groups.js";
 export async function appSwitch({ req, res, log }) {
 	let status = { ok: false, message: "no action" };
-	log("Body: ", ...req.body);
-
-	log(JSON.stringify(req.body));
+	log(typeof req.body);
 
 	try {
 		let payload = JSON.parse(req.body);
