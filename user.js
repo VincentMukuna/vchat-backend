30aa264ed4aace19198f92453773838ec2ffd8b3
Vincent Mukuna <mukunavincent28@gmail.com>
2023-09-21 22:41:11 +0300

    added check for undefined params

--- user.js
+++ user.js
@@ -9,6 +9,12 @@ export default { addContact, deleteContact, deleteUser };
 const { database: db, users: user } = api.provider();
 
 async function addContact({ adderDetailsID, addeeDetailsID }) {
+	if (
+		typeof adderDetailsID !== "string" ||
+		typeof addeeDetailsID !== "string"
+	) {
+		return { ok: false, message: "Invalid arguments!" };
+	}
 	try {
 		//get current contact details
 		let adderDetails = await db.getDocument(
