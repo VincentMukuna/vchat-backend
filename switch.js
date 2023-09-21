4e3620880c14d6f2dd2bdf4eada9dae76f498cf3
Vincent Mukuna <mukunavincent28@gmail.com>
2023-09-21 23:42:52 +0300

    w

--- switch.js
+++ switch.js
@@ -4,7 +4,7 @@ import { addToGlobalChat } from "./groups.js";
 export async function appSwitch({ req, res, log }) {
 	let status = { ok: false, message: "no action" };
 
-	log(req.body.action);
+	log(JSON.parse(req.body).action);
 
 	try {
 		let payload = JSON.parse(req.body);
