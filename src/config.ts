9ed47fcfac50703c351816a9087526a660bfff94
Vincent Mukuna <mukunavincent28@gmail.com>
2023-09-22 20:22:07 +0300

    migrated to typescript

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+//@ts-check
+export const Server = {
+	endpoint: "https://cloud.appwrite.io/v1",
+	projectID: "64cced226fa211528fbf",
+	databaseID: "production",
+	collectionIDChatMessages: "chat-messages",
+	collectionIDChats: "chats",
+	collectionIDUsers: "user-details",
+	collectionIDGroups: "groups",
+	collectionIDGroupMessages: "group-messages",
+	bucketIDAvatars: "user-avatars",
+	bucketIDAttachments: "chat-message-attachments",
+	documentIDGlobalChat: "global-chat",
+};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 //@ts-check
 import { appSwitch } from "./switch.js";
 
-export default async function ({ req, res, log }) {
+export default async function ({ req, res, log }: any) {
 	return appSwitch({ req, res, log });
 }
